14c8c00add18e5cde22370b89c0cf8dc5d40e03d
AmayaJuan <jpva360@gmail.com>
2018-04-25 13:56:23 -0500

    yuyf
    rry

--- app/src/main/java/com/example/jpva_/codigoqr/MainActivity.java
+++ app/src/main/java/com/example/jpva_/codigoqr/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.jpva_.codigoqr;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scanner Result");
+        builder.setMessage("Result "+ result.getText()+"\n" + "Format "+ result.getBarcodeFormat());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        scannerView.resumeCameraPreview(this);
+    }
+
+    public void scanner(View view){
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
     }
 }
