fb72e22ebbf290c481122ee5592f337d0140e4ce
AmayaJuan <jpva360@gmail.com>
2018-04-26 02:40:33 -0500

    yuuhh
    yyuujhj

--- app/src/main/java/com/example/jpva_/codigoqr/MainActivity.java
+++ app/src/main/java/com/example/jpva_/codigoqr/MainActivity.java
@@ -4,34 +4,71 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
+import com.imangazaliev.circlemenu.CircleMenu;
+import com.imangazaliev.circlemenu.CircleMenuButton;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView scannerView;
+ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleMenu circleMenu = (CircleMenu) findViewById(R.id.scannerQR);
+        circleMenu.setOnItemClickListener(new CircleMenu.OnItemClickListener() {
+            @Override
+            public void onItemClick(CircleMenuButton menuButton) {
+                switch (menuButton.getId()) {
+                    case R.id.image:
+                        showMessage("Image");
+                        break;
+                    case R.id.pdf:
+                        showMessage("Pdf");
+                        break;
+                    case R.id.play:
+                        showMessage("Play");
+                        break;
+                }
+            }
+        });
+
+        circleMenu.setStateUpdateListener(new CircleMenu.OnStateUpdateListener() {
+            @Override
+            public void onMenuExpanded() {
+                Log.d("CircleMenuStatus", "Expanded");
+            }
+
+            @Override
+            public void onMenuCollapsed() {
+                Log.d("CircleMenuStatus", "Collapsed");
+            }
+        });
+
+    }
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
-        scannerView.stopCamera();
+       super.onPause();
+       scannerView.stopCamera();
     }
-
-    @Override
+   @Override
     public void handleResult(Result result) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scanner Result");
-        builder.setMessage("Result "+ result.getText()+"\n" + "Format "+ result.getBarcodeFormat());
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-        scannerView.resumeCameraPreview(this);
+       AlertDialog.Builder builder = new AlertDialog.Builder(this);
+       builder.setTitle("Scanner Result");
+       builder.setMessage("Result "+ result.getText()+"\n" + "Format "+ result.getBarcodeFormat());
+       AlertDialog alertDialog = builder.create();
+       alertDialog.show();
+       scannerView.resumeCameraPreview(this);
     }
 
     public void scanner(View view){
@@ -39,5 +76,5 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         setContentView(scannerView);
         scannerView.setResultHandler(this);
         scannerView.startCamera();
-    }
+  }
 }
